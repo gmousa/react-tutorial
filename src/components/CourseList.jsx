af87a730001b0b2e7490b815e0f61f64199660e0
George Mousa <gmousa@dhcp-10-105-178-255.wireless.northwestern.private>
2023-10-09 22:41:01 -0500

    Added course cart modal

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,76 +1,52 @@
 import { useState } from "react";
+import TermSelector from './TermSelector';
+import CourseCard from './CourseCard';
+import { terms } from './Constants';
+import Modal from './Modal';
+import Cart from './Cart';
 
-const terms = {
-   Fall: 'Fall courses...',
-   Winter: 'Winter courses...',
-   Spring: 'Spring courses...'
-};
-
-const ProductPage = ({products}) => {
+const CourseList = ({ courses }) => {
   const [selected, setSelected] = useState([]);
-
-  const toggleSelected = (item) => setSelected(
-    selected.includes(item)
-    ? selected.filter(x => x !== item)
-    : [...selected, item]
-  );
-
-  return (
-    <ProductList products={products} selected={selected} toggleSelected={toggleSelected} />
-  );
-};
+  const [selection, setSelection] = useState(() => Object.keys(terms)[0]);
+  const selectedCourses = selected.map(courseId => courses[courseId]);
 
 
-const TermButton = ({term, selection, setSelection}) => (
-  <div>
-    <input type="radio" id={term} className="btn-check" checked={term === selection} autoComplete="off"
-      onChange={() => setSelection(term)} />
-    <label className="btn btn-success mb-1 p-2" htmlFor={term}>
-    { term }
-    </label>
-  </div>
-);
+  const [open, setOpen] = useState(false);
 
-const TermSelector = ({selection, setSelection}) => (
-  <div className="btn-group">
-    { 
-      Object.keys(terms).map(term => <TermButton key={term} term={term} selection={selection} setSelection={setSelection} />)
-    }
-  </div>
-);
-
-
-const CourseList = ({ courses }) => 
-{
-  const [selected, setSelected] = useState([]);
+  const openModal = () => setOpen(true);
+  const closeModal = () => setOpen(false);
 
   const toggleSelected = (course) => setSelected(
     selected.includes(course)
     ? selected.filter(x => x !== course)
     : [...selected, course]
   );
-  const [selection, setSelection] = useState(() => Object.keys(terms)[0]);
-  return (
-      <div>
-        <TermSelector selection={selection} setSelection={setSelection} />
-        <div className="course-cards">
-          {Object.entries(courses)
-          .filter(([id, course]) => course.term === selection)
-          .map(([id, { term, number, title, meets}]) => (        
-            <div className={`card m-1 p-2 ${selected.includes(id) ? 'selected' : ''}`} key = {id} onClick={() => toggleSelected(id)}>
-              <div className={`card-body`}>
-                <h5 className="card-title">{term} CS {number}</h5>
-                <p className="card-text">{title}</p>
-                <div className="mt-auto">
-                  <hr/>   
-                  <p className="card-text">{meets}</p>
-                </div>  
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
 
-  export default CourseList;
+  return (
+    <div className="course-container">
+      <TermSelector selection={selection} setSelection={setSelection} />
+      <button className="btn btn-outline-dark" onClick={openModal}>Selected Courses<i className="bi bi-cart4"></i></button>
+      <Modal open={open} close={closeModal}>
+        <Cart selected={selectedCourses} courses={courses} />
+      </Modal>
+      <div className="course-cards">
+        {Object.entries(courses)
+        .filter(([id, course]) => course.term === selection)
+        .map(([id, { term, number, title, meets}]) => (
+          <CourseCard 
+            key={id} 
+            id={id} 
+            term={term} 
+            number={number} 
+            title={title} 
+            meets={meets} 
+            selected={selected} 
+            toggleSelected={toggleSelected} 
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default CourseList;
