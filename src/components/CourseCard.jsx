2088df9e31ed2935f9470739f71737950c0a17d8
George Mousa <gmousa@Georges-MacBook-Air-2.local>
2023-11-02 23:04:18 -0500

    added content test

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -9,13 +9,14 @@ const CourseCard = ({ id, term, number, title, meets, selected, toggleSelected, 
 
   return(
     <div className={`card m-1 p-2 ${selected.includes(id) ? 'selected' : ''} ${isConflicting ? 'conflicting' : ''}`} 
+    data-cy="course"
     key={id}
     onClick={() => toggleSelected(id)}>
       <div className={`card-body`}>
         <h5 className="card-title">{term} CS {number}</h5>
         <p className="card-text">{title}</p>
         <div className="mt-auto">
-          <hr/>   
+          <hr/>     
           <p className="card-text">{meets}</p>
         </div>  
       </div>
